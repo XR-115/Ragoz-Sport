366720a4854ef25fe0999a30f3bc42012b188bb8
Johan Dario Garzon Maldonado <104364489+jdgm10@users.noreply.github.com>
2022-09-24 10:34:22 -0500

    vistas

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,268 +1,264 @@
-//----------------------------------ALERTAS PARA MÓDULO DE ENVÍO----------------------------------//
+$(function() {
+    
+    "use strict";
+    
+    //===== Prealoder
+    
+    $(window).on('load', function(event) {
+        $('.preloader').delay(500).fadeOut(500);
+    });
+    
+    
+    //===== Sticky
 
-//--------------------EDITAR---------------------//
-
-
-const editarEnvio=()=>{
- 
-  const swalWithBootstrapButtons = Swal.mixin({
-    customClass: {
-      confirmButton: 'btn btn-success',
-      cancelButton: 'btn btn-danger'
-    },
-    buttonsStyling: false
-  })
-  
-  swalWithBootstrapButtons.fire({
-    title: '¿Está seguro de que desea editar?',
-    text: "¿Usted va a editar este método de pago?",
-    icon: 'warning',
-    showCancelButton: true,
-    confirmButtonText: 'Si, editalo',
-    cancelButtonText: 'No, cancelar',
-    reverseButtons: true
-  }).then((result) => {
-    if (result.isConfirmed) {
-      swalWithBootstrapButtons.fire(
-        'Editando',
-        'Su método de pago se va a editar',
-        'success'
-      )
-    } else if (
-      window.location.href=`/ragoz/envio/envio`
-    ) {
-  
-      
-    }
-  })
-
-
-}
-//--------------------BORRAR---------------------//
-function borrarEnvio(idEnvio) {
-  let id=idEnvio;
-  Swal.fire({
-      title: '¿Está usted seguro?',
-      text: "No podrás revertir el cambio",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Si, ¡Borralo!'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        Swal.fire(
-          '¡Borrado!',
-          'Your file has been deleted.',
-          'success'
-
-        )
-        window.location.href=`deleteEn/${id}`
+    $(window).on('scroll', function (event) {
+        var scroll = $(window).scrollTop();
+        if (scroll < 20) {
+            $(".header_menu").removeClass("sticky");
+        } else {
+            $(".header_menu").addClass("sticky");
         }
-        
-    })
-}
+    });
+    
+    //===== Section Menu Active
 
-//----------------------------------ALERTAS PARA MÓDULO DE PAGO----------------------------------//
+    var scrollLink = $('.page-scroll');
+    // Active link switching
+    $(window).scroll(function () {
+        var scrollbarLocation = $(this).scrollTop();
 
-//--------------------EDITAR---------------------//
-const editarPago=()=>{
+        scrollLink.each(function () {
 
-const swalWithBootstrapButtons = Swal.mixin({
-  customClass: {
-    confirmButton: 'btn btn-success',
-    cancelButton: 'btn btn-danger'
-  },
-  buttonsStyling: false
-})
+            var sectionOffset = $(this.hash).offset().top - 73;
 
-swalWithBootstrapButtons.fire({
-  title: '¿Está seguro de que desea editar?',
-  text: "¿Usted va a editar este método de pago?",
-  icon: 'warning',
-  showCancelButton: true,
-  confirmButtonText: 'Si, editalo',
-  cancelButtonText: 'No, cancelar',
-  reverseButtons: true
-}).then((result) => {
-  if (result.isConfirmed) {
-    swalWithBootstrapButtons.fire(
-      'Editando',
-      'Su método de pago se va a editar',
-      'success'
-    )
-  } else if (
-    window.location.href=`/ragoz/pago/pago`
-  ) {
-  }
-})
+            if (sectionOffset <= scrollbarLocation) {
+                $(this).parent().addClass('active');
+                $(this).parent().siblings().removeClass('active');
+            }
+        });
+    });
+    
+    
+    //===== close navbar-collapse when a  clicked
 
-}
+    $("#menu_open").on('click', function () {
+        $(".side_menu_area").addClass('open');
+    });
 
-//
-//--------------------BORRAR---------------------//
-function borrarPago(idPago) {
-  let id=idPago;
-  Swal.fire({
-      title: '¿Está usted seguro?',
-      text: "No podrás revertir el cambio",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      cancelButtonText: 'Cancelar',
-      confirmButtonText: 'Si, ¡Borralo!'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        Swal.fire(
-          '¡Borrado!',
-          'Su método de pago fué eliminado.',
-          'Hecho'
+    $(".menu_close").on('click', function () {
+        $(".side_menu_area").removeClass('open');
+    });
+    
+    $(".side_menu a").on('click', function () {
+        $(".side_menu_area").removeClass("open");
+    });
+    
+    
+    //===== Slick Slider
 
-        )
-        window.location.href=`deleteP/${id}`
+    function mainSlider() {
+        var BasicSlider = $('.slider-active');
+        BasicSlider.on('init', function (e, slick) {
+            var $firstAnimatingElements = $('.single_slider:first-child').find('[data-animation]');
+            doAnimations($firstAnimatingElements);
+        });
+        BasicSlider.on('beforeChange', function (e, slick, currentSlide, nextSlide) {
+            var $animatingElements = $('.single_slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
+            doAnimations($animatingElements);
+        });
+        BasicSlider.slick({
+            autoplay: false,
+            autoplaySpeed: 10000,
+            dots: true,
+            fade: true,
+            arrows: true,
+            prevArrow: '<span class="prev"><i class="lni lni-chevron-left"></i></span>',
+            nextArrow: '<span class="next"><i class="lni lni-chevron-right"></i></span>',
+            responsive: [
+                {
+                    breakpoint: 767,
+                    settings: {
+                        arrows: false
+                    }
+                }
+            ]
+        });
+
+        function doAnimations(elements) {
+            var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+            elements.each(function () {
+                var $this = $(this);
+                var $animationDelay = $this.data('delay');
+                var $animationType = 'animated ' + $this.data('animation');
+                $this.css({
+                    'animation-delay': $animationDelay,
+                    '-webkit-animation-delay': $animationDelay
+                });
+                $this.addClass($animationType).one(animationEndEvents, function () {
+                    $this.removeClass($animationType);
+                });
+            });
         }
-        
-    })
-}
-//-------------------------------------- ALERTAS PARA MÓDULO DE PRODUCTO ----------------------------------//
-
-//--------------------EDITAR---------------------//
-const editarProducto=()=>{
-
-const swalWithBootstrapButtons = Swal.mixin({
-  customClass: {
-    confirmButton: 'btn btn-success',
-    cancelButton: 'btn btn-danger'
-  },
-  buttonsStyling: false
-})
-
-swalWithBootstrapButtons.fire({
-  title: '¿Está seguro de que desea editar?',
-  text: "¿Usted va a editar este método de pago?",
-  icon: 'warning',
-  showCancelButton: true,
-  confirmButtonText: 'Si, editalo',
-  cancelButtonText: 'No, cancelar',
-  reverseButtons: true
-}).then((result) => {
-  if (result.isConfirmed) {
-    swalWithBootstrapButtons.fire(
-      'Editando',
-      'Su método de pago se va a editar',
-      'success'
-    )
-  } else if (
-    window.location.href=`/ragoz/producto/producto`
-  ) {
-  }
-})
-
-}
-
-
-//-------------------------------------- ALERTAS PARA MÓDULO DE USUARIOS ----------------------------------//
-
-//-------------Actualizar-------------//
-const editarUsuario=()=>{
-
-const swalWithBootstrapButtons = Swal.mixin({
-  customClass: {
-    confirmButton: 'btn btn-success',
-    cancelButton: 'btn btn-danger'
-  },
-  buttonsStyling: false
-})
-
-swalWithBootstrapButtons.fire({
-  title: '¿Está seguro de que desea editar?',
-  text: "¿Usted va a editar este método de pago?",
-  icon: 'warning',
-  showCancelButton: true,
-  confirmButtonText: 'Si, editalo',
-  cancelButtonText: 'No, cancelar',
-  reverseButtons: true
-}).then((result) => {
-  if (result.isConfirmed) {
-    swalWithBootstrapButtons.fire(
-      'Editando',
-      'Su método de pago se va a editar',
-      'success'
-    )
-  } else if (
-    window.location.href=`/ragoz/usuario/usuario`
-  ) {
-  }
-})
-
-}
-
-//-------------------------------------- ALERTAS PARA MÓDULO DE PEDIDO ----------------------------------//
-const editarPedido=()=>{
-
-const swalWithBootstrapButtons = Swal.mixin({
-  customClass: {
-    confirmButton: 'btn btn-success',
-    cancelButton: 'btn btn-danger'
-  },
-  buttonsStyling: false
-})
-
-swalWithBootstrapButtons.fire({
-  title: '¿Está seguro de que desea editar?',
-  text: "¿Usted va a editar este método de pago?",
-  icon: 'warning',
-  showCancelButton: true,
-  confirmButtonText: 'Si, editalo',
-  cancelButtonText: 'No, cancelar',
-  reverseButtons: true
-}).then((result) => {
-  if (result.isConfirmed) {
-    swalWithBootstrapButtons.fire(
-      'Editando',
-      'Su método de pago se va a editar',
-      'success'
-    )
-  } else if (
-    window.location.href=`/ragoz/pedido/pedido`
-  ) {
-  }
-})
-
-}
-
-//-------------------------------------- ALERTAS PARA MÓDULO DE PEDIDO ----------------------------------//
-
-const editarGuia=()=>{
-
-  const swalWithBootstrapButtons = Swal.mixin({
-    customClass: {
-      confirmButton: 'btn btn-success',
-      cancelButton: 'btn btn-danger'
-    },
-    buttonsStyling: false
-  })
-  
-  swalWithBootstrapButtons.fire({
-    title: '¿Está seguro de que desea editar?',
-    text: "¿Usted va a editar este método de pago?",
-    icon: 'warning',
-    showCancelButton: true,
-    confirmButtonText: 'Si, editalo',
-    cancelButtonText: 'No, cancelar',
-    reverseButtons: true
-  }).then((result) => {
-    if (result.isConfirmed) {
-      swalWithBootstrapButtons.fire(
-        'Editando',
-        'Su método de pago se va a editar',
-        'success'
-      )
-    } else if (
-      window.location.href=`/ragoz/guia/listar`
-    ) {
     }
-  })
-  
-  }
+    mainSlider();
+    
+    
+    //====== Magnific Popup
+    
+    $('.video-popup').magnificPopup({
+        type: 'iframe'
+        // other options
+    });
+    
+    
+    //===== Magnific Popup
+    
+    $('.image-popup').magnificPopup({
+      type: 'image',
+      gallery:{
+        enabled:true
+      }
+    });
+    
+    
+    //===== Counter Up
+    
+    $('.counter').counterUp({
+        delay: 10,
+        time: 3000
+    });
+    
+    
+    //===== Slick Project
+    
+    $('.project_active').slick({
+        dots: false,
+        infinite: true,
+        speed: 800,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        arrows: true,
+        prevArrow: '<span class="prev"><i class="lni lni-chevron-left"></i></span>',
+        nextArrow: '<span class="next"><i class="lni lni-chevron-right"></i></span>',
+        responsive: [
+            {
+              breakpoint: 1400,
+              settings: {
+                slidesToShow: 4,
+              }
+            },
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 4,
+              }
+            },
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: 3,
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2,
+              }
+            },
+            {
+              breakpoint: 576,
+              settings: {
+                slidesToShow: 1,
+              }
+            }
+        ]
+    });
+    
+    
+    //===== Slick Testimonial
+    
+    $('.testimonial_active').slick({
+        dots: true,
+        infinite: true,
+        speed: 800,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: false,
+        responsive: [
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 2,
+              }
+            },
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: 2,
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1,
+              }
+            },
+            {
+              breakpoint: 576,
+              settings: {
+                slidesToShow: 1,
+              }
+            }
+        ]
+    });    
+    
+    
+    //===== Back to top
+    
+    // Show or hide the sticky footer button
+    $(window).on('scroll', function(event) {
+        if($(this).scrollTop() > 600){
+            $('.back-to-top').fadeIn(200)
+        } else{
+            $('.back-to-top').fadeOut(200)
+        }
+    });
+    
+    
+    //Animate the scroll to yop
+    $('.back-to-top').on('click', function(event) {
+        event.preventDefault();
+        
+        $('html, body').animate({
+            scrollTop: 0,
+        }, 1500);
+    });
+    
+    
+    //=====  WOW active
+    
+    var wow = new WOW({
+        boxClass: 'wow', //
+        mobile: false, // 
+    })
+    wow.init();
+    
+    
+    //===== 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+});
