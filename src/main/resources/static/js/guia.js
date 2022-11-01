22cf5bcf97a126f0dd8472540309a7f9022abac4
Juan Quitian <yaparada96@misena.edu.co>
2022-11-01 12:05:30 -0500

    update

--- src/main/resources/static/js/guia.js
+++ src/main/resources/static/js/guia.js
@@ -11,3 +11,23 @@ function deshabilitarFechaAnterior() {
     console.log(texto);
 }
 fechaInput.addEventListener('click',deshabilitarFechaAnterior);
+
+//SOLO LETRAS INPUT//
+function sololetras(e){
+    key=e.keyCode || e.which;
+    teclado=String.fromCharCode(key).toLowerCase();
+    letras=" abcdefghijklmnñopqrstuvwxyz";
+    especiales="8-37-38-46-164";
+    teclado_especial=false;
+
+for(var i in especiales){
+    if(key==especiales[i]){
+        teclado_especial=true;break;
+    }
+}
+
+if(letras.indexOf(teclado)==-1 && !teclado_especial){
+    return false;
+}
+
+}
